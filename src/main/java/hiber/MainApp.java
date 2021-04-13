3f1dfe1f6e962376b10493fa8882b09361b03f64
Nikolay Rednov <red.niko@yandex.ru>
2021-04-13 22:49:05 +0400

    done

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,16 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car = new Car("UAZ",22);
+      Car car2 = new Car("VAZ",15);
+      Car car3= new Car("KAMAZ",31);
+      Car car4 = new Car("TAGAZ",47);
+
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",car));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +33,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("car = "+user.getCar());
          System.out.println();
       }
+      System.out.println(userService.getUserByCar("VAZ",15));
+
+
 
       context.close();
    }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -6,6 +6,14 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   @OneToOne(optional = false, cascade = CascadeType.ALL)
+   @JoinColumn(name="car_id")
+   private Car car;
+
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
@@ -19,12 +27,26 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   public  Car getCar() {
+      return car;
+   }
+
    public User() {}
-   
-   public User(String firstName, String lastName, String email) {
+
+   public String toString() {
+       return "User {" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
